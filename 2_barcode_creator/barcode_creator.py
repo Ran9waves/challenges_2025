95227be4c130a654232e6b353a71198d3b950463
Ran9waves <ran9waves@protonmail.com>
2025-04-16 19:50:03 +0100

    barcode program

---
+++ 2_barcode_creator/barcode_creator.py
@@ -0,0 +1,10 @@
+import barcode
+from barcode.writer import ImageWriter
+from IPython.display import Image, display
+
+barcode_format = barcode.get_barcode_class('ean13')
+barcode_number = "880445671453"
+barcode_image = barcode_format(barcode_number, writer=ImageWriter())
+barcode_filename = 'barcode_image'
+barcode_image.save(barcode_filename)
+display(Image(filename='f{barcode_filename}.png'))
